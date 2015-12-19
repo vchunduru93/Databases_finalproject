086412f3ed2d000062abeed2171d9c44402f8806
Vamsi Chunduru <vchundu1@ugrad6.cs.jhu.edu>
2015-12-19 16:37:58 -0500

    Changed mysqltest.py to Venus's database

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Databases_finalproject
 Final Project for Database Systems (Yarowsky) Fall 2015
+Interactive Course Evaluation Tool
+
+Venus Soontornprueksa (315)
+Vamsi Chunduru (415)
--- mysqltest.py
+++ mysqltest.py
@@ -1,6 +1,9 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='cs41515_vchundu1', password='ZESHEHOW',
+cnx = mysql.connector.connect(user='cs41515_vsoonto1', password='SHRQKPKD',
                               host='dbase.cs.jhu.edu',
-                              database='cs41515_vchundu1_db')
+                              database='cs41515_vsoonto1_db')
+
+query = ("")
+
 cnx.close()
